35e8ba65ccbb1b27da3a7cac7d750f5992c2e7c7
awais3344 <awaisalishah02@gmail.com>
2025-01-31 23:58:42 +0500

    dependencies fixes

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Simple Add function
+"""
+
+def add(x, y):
+    """Add two numbers together"""
+    return x + y
+
+def subtract(x, y):
+    """Subtract x from y and return value"""
+    return y - x
---
+++ app/app/tests.py
@@ -0,0 +1,16 @@
+"""
+Simple Test Case for add function
+"""
+from django.test import SimpleTestCase
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    def test_add(self):
+        self.assertEqual(calc.add(3, 8), 11)
+    
+    #TDD ( TEST DRIVEN DEVELOPMENT)
+
+    def test_subtract(self):
+        self.assertEqual(calc.subtract(5, 11), 6)
+        
