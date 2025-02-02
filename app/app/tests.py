270a8ed840d39eb4c78b1679aa203bb9730ccdd3
awais3344 <awaisalishah02@gmail.com>
2025-02-03 03:02:37 +0500

    Bug Fixes

--- app/app/tests.py
+++ app/app/tests.py
@@ -8,8 +8,6 @@ from app import calc
 class CalcTests(SimpleTestCase):
     def test_add(self):
         self.assertEqual(calc.add(3, 8), 11)
-    
-#TDD( TEST DRIVEN DEVELOPMENT)
-
+        
     def test_subtract(self):
         self.assertEqual(calc.subtract(5, 11), 6)
