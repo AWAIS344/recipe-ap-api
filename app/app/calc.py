86bc2d9599f0a6b761ffa8a810c235715d9314f3
awais3344 <awaisalishah02@gmail.com>
2025-02-03 03:06:33 +0500

    Bug Fixes

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,10 +3,12 @@ Simple Add function
 """
 
 def add(x, y):
+
     """Add two numbers a and b and return the result"""
     return x + y
 
 def subtract(x, y):
+    
     """Subtract x from y and return value"""
     return y - x
 
